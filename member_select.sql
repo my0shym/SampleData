6e132e35303897eb29a2d31bc1478e316f2244a2
my0shym <benzenetarou@gmail.com>
2020-08-28 20:26:34 +0900

    add select data

---
+++ member_select.sql
@@ -0,0 +1,15 @@
+select "tanaka" as name,20 as age,"engineer" as job,400 as salary
+union all select "yamada",21,"sales",350
+union all select "takada",22,"engineer",600
+union all select "yamana",30,"sales",450
+union all select "suzuki",35,"engineer",200
+union all select "kato",60,"sales",333
+union all select "ito",59,"sales",750
+union all select "sudo",39,"engineer",1000
+union all select "maeda",44,"sales",800
+union all select "sato",33,"admin",200
+union all select "yoshida",33,"admin",400
+union all select "sasaki",54,"admin",350
+union all select "kimura",39,"engineer",700
+union all select "hayashi",58,"admin",650
+union all select "saito",44,"admin",500
